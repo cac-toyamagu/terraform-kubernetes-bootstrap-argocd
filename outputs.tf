b0e191d98f4202867637583b4791188ef438377c
toyamagu <toyamagu2021@gmail.com>
2022-09-15 12:26:57 +0900

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "argocd_apps_helm_values" {
-  value = local.argocd_apps.helm_values
+  description = "Merged helm values for argocd-apps. JSON encoded."
+  value       = helm_release.argocd_apps.metadata[0].values
 }
 
 output "argocd_helm_values" {
-  value = local.argocd.helm_values
+  description = "Merged helm values for argocd. JSON encoded."
+  value       = helm_release.argocd.metadata[0].values
 }
