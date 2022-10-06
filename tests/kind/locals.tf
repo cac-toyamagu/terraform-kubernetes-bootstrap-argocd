0eb6a61d09101b014f3caac2e5eb08df19fb636d
toyamagu <toyamagu2021@gmail.com>
2022-10-06 22:09:12 +0900

    applicationsets

--- tests/kind/locals.tf
+++ tests/kind/locals.tf
@@ -2,7 +2,12 @@ locals {
   kind = {
     cluster_name = var.kind_cluster_name
   }
-  helm_values = var.read_local_helm_values ? [templatefile("${path.module}/values.yaml", {})] : []
+  helm_values_vars = {
+    repo_url        = var.argocd_apps_repo
+    target_revision = var.argocd_apps_target_revision
+    path            = "tests/kind/argocd/kustomize-guestbook/*"
+  }
+  helm_values = var.read_local_helm_values ? [templatefile("${path.module}/values.yaml", local.helm_values_vars)] : []
   argocd_applications_vars = {
     repo_url        = var.argocd_apps_repo
     target_revision = var.argocd_apps_target_revision
